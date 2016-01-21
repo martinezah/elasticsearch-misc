96c51d04c09173b5f6c2faadc971035d54217919
Marti Martinez <martinezah@gmail.com>
2016-01-21 21:13:16 +0000

    adding argument parser

--- purge/purge.py
+++ purge/purge.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
 
+import argparse
 import datetime
 import requests
 import sys
 import time
 
-ES_URL = ''
+ES_URL = 'http://localhost:9200'
 ES_REPO = ''
 ES_USER = ''
 ES_PASS = ''
+RETENTION_DAYS = 14
 
-MAX_AGE = 14
-
+# load settings
 try:
     from local_settings import *
 except:
     pass
+
+# process commandline args
+parser = argparse.ArgumentParser(description='Purge elasticsearch snapshots')
+parser.add_argument('--url', help='Elasticsearch API URL')
+parser.add_argument('--username', help='Elasticsearch HTTP basic auth username')
+parser.add_argument('--password', help='Elasticsearch HTTP basic auth username')
+parser.add_argument('--repository', help='Elasticsearch snapshot repository name')
+parser.add_argument('--days', help='Number of days to retain snapshots')
+args = parser.parse_args()
+
+if args.url:
+    ES_URL = args.url
+if args.username:
+    ES_USER = args.username
+if args.password:
+    ES_PASS = args.password
+if args.repository:
+    ES_REPO = args.repository
+if args.days:
+    RETENTION_DAYS = int(args.days)
 
 counts = {'total': 0, 'purged': 0, 'retained': 0, 'failed': 0}
 
@@ -46,7 +67,7 @@ print 'Found {} snapshots; last successful snapshot was {}'.format(counts['total
     
 # if most recent success is within age range, delete all snapshots outside age range
 # otherwise, delete all snapshots prior to most recent success
-purge_date = int(time.time()*1000) - (MAX_AGE * 24 * 60 * 60 * 1000)
+purge_date = int(time.time()*1000) - (RETENTION_DAYS * 24 * 60 * 60 * 1000)
 if last_success < purge_date:
     purge_date = last_success
     print "WARNING: no successful snapshots in retention range"
